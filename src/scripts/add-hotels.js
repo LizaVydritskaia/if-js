d396e1dd313de4dd92ea1c86ba7792345351e56a
LizaVydritskaia <liza.vydritkaia@gmail.com>
2023-03-02 22:21:41 +0300

    Fixed script that add hotels to "Homes guests loves" block

--- src/scripts/add-hotels.js
+++ src/scripts/add-hotels.js
@@ -1,22 +1,28 @@
 const homesHotels = document.getElementById('hotels');
 
-fetch('https://if-student-api.onrender.com/api/hotels/popular')
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
-    return data;
-  })
-  .then((hotelsData) => {
-    return hotelsData
-      .map(
-        (
-          item,
-        ) => `<div class="col-lg-3 col-md-6 col-sm-3 homes__hotel-block"><img class="homes__hotels-image" src=${item.imageUrl} alt=${item.name}/>
+const getData = async () => {
+  const data = await fetch(
+    'https://if-student-api.onrender.com/api/hotels/popular',
+  );
+  return await data.json();
+};
+
+const getHotels = async () => {
+  await getData()
+    .then((hotelsData) => {
+      return hotelsData
+        .map(
+          (
+            item,
+          ) => `<div class="col-lg-3 col-md-6 col-sm-3 homes__hotel-block"><img class="homes__hotels-image" src=${item.imageUrl} alt=${item.name}/>
 <p class="homes__hotel-name">${item.name}</p>
 <p class="homes__hotel-location">${item.city}, ${item.country}</p></div>`,
-      )
-      .join('');
-  })
-  .then((hotelsItem) => {
-    homesHotels.insertAdjacentHTML('afterbegin', hotelsItem);
-  });
+        )
+        .join('');
+    })
+    .then((hotelsItem) => {
+      homesHotels.insertAdjacentHTML('afterbegin', hotelsItem);
+    });
+};
+
+getHotels();
