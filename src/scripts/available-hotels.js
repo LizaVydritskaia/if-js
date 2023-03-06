cac381fbe674c59150846bd89f8aa0d2fee07c6a
LizaVydritskaia <liza.vydritkaia@gmail.com>
2023-03-06 21:53:16 +0300

    Added "Available hotels" block to project using fetch and async function

---
+++ src/scripts/available-hotels.js
@@ -0,0 +1,36 @@
+const searchButton = document.querySelector('.top-section__button');
+const sectionAvailableHotels = document.querySelector('.available-hotels');
+const availableHotels = document.getElementById('available-hotels');
+
+const getSearchData = async () => {
+  const destinationInputValue = document.getElementById('destination').value;
+
+  const searchData = await fetch(
+    'https://if-student-api.onrender.com/api/hotels?' +
+      new URLSearchParams({ search: destinationInputValue }).toString(),
+  );
+
+  return searchData.json();
+};
+
+const renderAvailableHotels = async () => {
+  sectionAvailableHotels.style.display = 'block';
+
+  await getSearchData()
+    .then((hotels) => {
+      return hotels
+        .map(
+          (
+            item,
+          ) => `<div class="col-lg-3 col-md-6 col-sm-3 available-hotels__hotel-block"><img class="homes__hotels-image" src=${item.imageUrl} alt=${item.name}/>
+<p class="homes__hotel-name">${item.name}</p>
+<p class="homes__hotel-location">${item.city}, ${item.country}</p></div>`,
+        )
+        .join('');
+    })
+    .then((hotelBlock) => {
+      availableHotels.insertAdjacentHTML('afterbegin', hotelBlock);
+    });
+};
+
+searchButton.addEventListener('click', renderAvailableHotels);
