d11e9421dc083cfad8b27fc9ffafd98377272e7c
LizaVydritskaia <liza.vydritkaia@gmail.com>
2023-03-29 19:24:31 +0300

    Added function to hide filter

--- src/scripts/showFilter.js
+++ src/scripts/showFilter.js
@@ -1,8 +1,15 @@
 const adultsChildrenRoomInput = document.querySelector('.top-section__room');
+const searchButton = document.querySelector('.top-section__button');
 const filter = document.querySelector('.top-section__filter');
 
 const showFilter = () => {
   filter.style.display = 'block';
 };
 
+const hideFilter = () => {
+  filter.style.display = 'none';
+};
+
 adultsChildrenRoomInput.addEventListener('click', showFilter);
+
+searchButton.addEventListener('click', hideFilter);
